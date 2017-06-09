4b0d1d29786e9596fe1947d4f708838b6973de05
guiquan.chen <guiquan.chen@GZCDC101248.dir.svc.accenture.com>
2017-06-09 17:22:48 +0800

    commit

---
+++ src/main/java/org/springboot/SpringBootAjaxApplication.java
@@ -0,0 +1,19 @@
+package org.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Intellij IDEA.
+ *
+ * @Author LUOLIANG
+ * @Date 2016/8/2
+ * @Comment
+ */
+@SpringBootApplication
+public class SpringBootAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAjaxApplication.class, args);
+	}
+}
